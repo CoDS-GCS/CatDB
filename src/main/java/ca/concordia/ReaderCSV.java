2102cec102f33115eaa9ccd51520f0695ae84557
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2023-10-20 00:26:23 -0400

    Init Commit

---
+++ src/main/java/ca/concordia/ReaderCSV.java
@@ -0,0 +1,15 @@
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class myMain {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        SparkSession spark = SparkSession.builder().appName("csv-demo").getOrCreate();
+        Dataset<Row> employees = spark.read().csv("/home/saeed/Documents/Github/CatDB/Experiments/data/IMDB/ImdbName.csv");
+        employees.printSchema();
+        employees.show();
+        //employees.write().json("file:///home/rahul/Desktop/eclipse-ee/employees_spark.json");
+    }
+}

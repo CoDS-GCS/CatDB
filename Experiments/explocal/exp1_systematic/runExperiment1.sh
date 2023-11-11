9fc2d660586b2b7f7552ba45acd2f1a88f463b83
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2023-11-11 00:36:46 -0500

    Experiment 1

--- Experiments/explocal/exp1_systematic/runExperiment1.sh
+++ Experiments/explocal/exp1_systematic/runExperiment1.sh
@@ -6,10 +6,10 @@ dataset=$1
 target_attribute=$2
 log_file_name=$3
 
-for time_left in 60 120 180 240 300 360 420 480 540 600
+for time_left in 120 180 240 300
 do
 
-    for per_run_time_limit in 10 20 30 40 50 60
+    for per_run_time_limit in 10 20 30
     do
 
         ./explocal/exp1_systematic/runExperiment1_AutoML_Classifier.sh  SKLearn_Classifier_AutoML ${dataset} ${target_attribute} ${time_left} ${per_run_time_limit} ${log_file_name}

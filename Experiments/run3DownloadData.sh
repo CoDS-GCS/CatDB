b5bc52c90e8dd585ff394aa109db604c623665b7
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2023-12-28 20:45:14 -0500

    Remove KGLiDS from CatDB

--- Experiments/run3DownloadData.sh
+++ Experiments/run3DownloadData.sh
@@ -2,20 +2,15 @@
 root_path="$(pwd)"
 data_path="${root_path}/data"
 
-# Enable venv for download datasets and resources
+# Enable venv for download datasets and resources_m
 cd ${data_path}
 rm -rf venv
 python -m venv venv
 source venv/bin/activate
 
-# Download fasttext pre-trained models and save it in src/main/python/resources path
-cd $root_path
-cd ..
-
-cd src/main/python/resources
-python DownloadFasttext.py # Run python script for download models
-
-cd ${data_path} # Got to data directory and download experiment datasets
+# install packages
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 ls -hl
 python -m pip install --upgrade pip
--- src/main/python/resources/FastTextEmbeddingModel.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-
-def get_fast_text_embedding_model_path():
-    return str(Path(__file__).parent) + '/cc.en.50.bin'

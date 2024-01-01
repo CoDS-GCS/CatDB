215185edb9b2634bfc30c70c0870ba2c1becca12
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-01-01 15:31:15 -0500

    Minor

--- Experiments/explocal/exp1_systematic/runExperiment1_Data_Profile.sh
+++ Experiments/explocal/exp1_systematic/runExperiment1_Data_Profile.sh
@@ -7,6 +7,7 @@ exp_path="$(pwd)"
 data_source="${exp_path}/data"
 data_profile_path="${exp_path}/setup/Baselines/kglids/kg_governor/data_profiling/src/"
 
+eval "$(conda shell.bash hook)"
 conda activate kglids
 
 cd ${data_profile_path}

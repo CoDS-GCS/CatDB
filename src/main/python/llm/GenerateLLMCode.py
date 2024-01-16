64165b3ddb03ba22f19f129030bb8fc08f236299
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-01-16 11:06:18 -0500

    minor

--- src/main/python/llm/GenerateLLMCode.py
+++ src/main/python/llm/GenerateLLMCode.py
@@ -15,8 +15,7 @@ def generate_llm_code(model: str, prompt: BasicICLPrompt):
 
 
 def generate_GPT_LLM_code(model: str, prompt: BasicICLPrompt):
-
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"),)
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"), )
 
     if model == "gpt-4":
         model_token_limit = 8192
@@ -25,8 +24,7 @@ def generate_GPT_LLM_code(model: str, prompt: BasicICLPrompt):
     else:
         model_token_limit = -1
 
-
-    max_token_size = model_token_limit * 0.8
+    max_token_size = int(model_token_limit * 0.8)
     enc = tiktoken.get_encoding("cl100k_base")
     enc = tiktoken.encoding_for_model(model)
     prompt_format = prompt.format()
@@ -45,10 +43,13 @@ def generate_GPT_LLM_code(model: str, prompt: BasicICLPrompt):
     token_integers = enc.encode(prompt_text)
     num_tokens = len(token_integers)
 
-    system_rules = ['You will be given a dataset, a schema of the dataset, some extra information, and a question. Your task is to generate a data science pipeline. You should answer only by generating code. You should follow Steps 1 to 11 to answer the question. You should return a data science pipeline in Python 3.10 programming language. If you do not have a relevant answer to the question, simply write: "Insufficient information."']
+    system_rules = [
+        'You will be given a dataset, a schema of the dataset, some extra information, and a question. Your task is to generate a data science pipeline. You should answer only by generating code. You should follow Steps 1 to 11 to answer the question. You should return a data science pipeline in Python 3.10 programming language. If you do not have a relevant answer to the question, simply write: "Insufficient information."']
     if num_tokens >= model_token_limit:
-        system_rules.append('the user will supply text, preceded by the prefix "[START PART 1/10]" and followed by the suffix "[END PART 1/10]" (1: refer to first pat, and 10: is the total parts). For instance, \n[START PART 1/10]\nthis is the content of part 1 out of 10.\n[END PART 1/10].')
-        system_rules.append('when user tell you "ALL PARTS SENT", then you can continue processing the data and answering user\'s requests.')
+        system_rules.append(
+            'the user will supply text, preceded by the prefix "[START PART 1/10]" and followed by the suffix "[END PART 1/10]" (1: refer to first pat, and 10: is the total parts). For instance, \n[START PART 1/10]\nthis is the content of part 1 out of 10.\n[END PART 1/10].')
+        system_rules.append(
+            'when user tell you "ALL PARTS SENT", then you can continue processing the data and answering user\'s requests.')
 
     system_rules.extend([
         'the user will provide the path of the training and test data enclosed in triple quotes. For the training data, use """train_data=path/to/train/dataset""", and for the test data, use """test_data=path/to/test/dataset""". Load the datasets using pandas\' CSV readers.',
@@ -65,7 +66,7 @@ def generate_GPT_LLM_code(model: str, prompt: BasicICLPrompt):
     step_index = 1
     for m in system_rules:
         system_message += f"Step {step_index}: {m} \n\n"
-        step_index +=1
+        step_index += 1
 
     messages = [
         {
@@ -91,14 +92,16 @@ def generate_GPT_LLM_code(model: str, prompt: BasicICLPrompt):
         for chunk in chunks:
             message_text = []
             if c < chunk_len:
-                message_text.append(f'Do not answer yet. This is just another part of the text I want to send you. Just recive and acknowledge a "Part {c}/{chunk_len} received" and wait for the next part.')
+                message_text.append(
+                    f'Do not answer yet. This is just another part of the text I want to send you. Just recive and acknowledge a "Part {c}/{chunk_len} received" and wait for the next part.')
 
             message_text.append(f'[START PART {c}/{len(chunks)}]')
             message_text.append(f'{chunk}')
             message_text.append(f'[END PART {c}/{len(chunks)}]\n')
 
             if c < chunk_len:
-                message_text.append(f'Remember not answer yet. Just acknowledge you recived this part with the message "Part {c}/{chunk_len} received" and wait for the next part.')
+                message_text.append(
+                    f'Remember not answer yet. Just acknowledge you recived this part with the message "Part {c}/{chunk_len} received" and wait for the next part.')
             if c == chunk_len:
                 message_text.append("ALL PARTS SENT. Now you can continue processing the rquest.")
 
@@ -106,14 +109,14 @@ def generate_GPT_LLM_code(model: str, prompt: BasicICLPrompt):
             message_text_all.append(message_text)
 
             messages = [{"role": "user", "content": message_text}]
-            #codes.append(send_Message_to_GPT(client=client, messages=messages, model=model))
+            # codes.append(send_Message_to_GPT(client=client, messages=messages, model=model))
             c += 1
-        code = "" # TODO: fix large message problem
+        code = ""  # TODO: fix large message problem
         print(f"#### {extra_info['data_source_train_path']} #### DON'T SEND!")
         return code, system_message, "\n".join(message_text_all)
     else:
-        #prompt_text = f'[START PART 1/1]\n{prompt_text}\n[END PART 1/1]\n[ALL PARTS SENT]'
-        messages.append({"role": "user", "content": prompt_text,})
+        # prompt_text = f'[START PART 1/1]\n{prompt_text}\n[END PART 1/1]\n[ALL PARTS SENT]'
+        messages.append({"role": "user", "content": prompt_text, })
         code = send_Message_to_GPT(messages=messages, client=client, model=model)
         return code, system_message, prompt_text
 

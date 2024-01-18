a091276c4734a3d3442cb5799f84181ea44362de
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-01-18 01:54:29 -0500

    minor

--- Experiments/explocal/exp1_catalog/runExperiment1.sh
+++ Experiments/explocal/exp1_catalog/runExperiment1.sh
@@ -6,21 +6,21 @@ task_type=$2
 exp_path="$(pwd)"
 
 # Run Data Profiling Experiments
-./explocal/exp1_catalog/runExperiment1_Data_Profile.sh $dataset
+#./explocal/exp1_catalog/runExperiment1_Data_Profile.sh $dataset
 
 # Rnn CSV Data Reader Experiment
-./explocal/exp1_catalog/runExperiment1_CSVDataReader.sh $dataset
+#./explocal/exp1_catalog/runExperiment1_CSVDataReader.sh $dataset
 
 # Run Prompt and LLM Pipeline Generation Experiments
 
 #SCHEMA
 #######
-#./explocal/exp1_catalog/runExperiment1_LLM_Pipe_Gen.sh ${dataset} SCHEMA Random 0 1 ${task_type} gpt-4
+./explocal/exp1_catalog/runExperiment1_LLM_Pipe_Gen.sh ${dataset} SCHEMA Random 0 1 ${task_type} gpt-4
 #./explocal/exp1_catalog/runExperiment1_LLM_Pipe_Gen.sh ${dataset} SCHEMA Random 0 1 ${task_type} gpt-3.5-turbo
 
 
 #SCHEMA_STATISTIC
 #################
-#./explocal/exp1_catalog/runExperiment1_LLM_Pipe_Gen.sh ${dataset} SCHEMA_STATISTIC Random 0 1 ${task_type} gpt-4
+./explocal/exp1_catalog/runExperiment1_LLM_Pipe_Gen.sh ${dataset} SCHEMA_STATISTIC Random 0 1 ${task_type} gpt-4
 #./explocal/exp1_catalog/runExperiment1_LLM_Pipe_Gen.sh ${dataset} SCHEMA_STATISTIC Random 0 1 ${task_type} gpt-3.5-turbo
 

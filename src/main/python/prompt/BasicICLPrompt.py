0675da6e902ecd8441ddb3ad5f6f8134cdebe0ad
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-02-01 12:32:10 -0500

    Reconstruct Project

--- src/main/python/prompt/BasicICLPrompt.py
+++
@@ -1,9 +0,0 @@
-class BasicICLPrompt(object):
-    NUM_EXAMPLE = None
-    SEP_EXAMPLE = "\n\n"
-
-    def __init__(self, *args, **kwargs):
-        self.example_qualities = []
-
-    def format(self, examples: dict):
-        return self.format_target(examples)
--- src/test/TestCatalog.py
+++
@@ -1,5 +0,0 @@
-from src.main.python.catalog.Catalog import load_data_source_profile
-
-if __name__ == '__main__':
-    cat = load_data_source_profile(data_source_path="/home/saeed/projects/kglids/storage/profiles/tus_profiles_fine_grained/", file_format="JSON")
-    a = 100

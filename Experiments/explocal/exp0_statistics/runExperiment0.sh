9fe9b1c010bde6c7c49663050f3e78d74595fb87
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-04-13 17:17:02 -0400

    minor update

--- Experiments/explocal/exp0_statistics/runExperiment0.sh
+++ Experiments/explocal/exp0_statistics/runExperiment0.sh
@@ -4,15 +4,20 @@ dataset=$1
 
 
 exp_path="$(pwd)"
+data_path="${exp_path}/data"
 statistics_path="${exp_path}/results/statistics"
 mkdir -p ${statistics_path}
 
 log_file_name="${exp_path}/results/statistics/dataset_overview.dat"
 statistic_file_name="${exp_path}/results/statistics/${dataset}_statistics.dat"
 
+# Run Data Profiling
+data_profile_path="${data_path}/${dataset}/data_profile_full"
+./explocal/exp0_statistics/runExperiment0_Data_Profile.sh ${dataset} ${data_profile_path}
+
 # CatDB
-./explocal/exp0_statistics/runExperiment0_CatDB.sh ${dataset} gpt-4-turbo Yes ${log_file_name} ${statistic_file_name}
-./explocal/exp0_statistics/runExperiment0_CatDB.sh ${dataset} gpt-4-turbo No ${log_file_name} ${statistic_file_name}
+./explocal/exp0_statistics/runExperiment0_CatDB.sh ${dataset} gpt-4-turbo Yes ${log_file_name} ${statistic_file_name} ${data_profile_path}
+./explocal/exp0_statistics/runExperiment0_CatDB.sh ${dataset} gpt-4-turbo No ${log_file_name} ${statistic_file_name} ${data_profile_path}
 
 # CAAFE
 ./explocal/exp0_statistics/runExperiment0_CAAFE.sh ${dataset} gpt-4-turbo Yes ${log_file_name}
--- Experiments/explocal/exp0_statistics/runExperiment0_CatDB.sh
+++ Experiments/explocal/exp0_statistics/runExperiment0_CatDB.sh
@@ -4,15 +4,11 @@ dataset=$1
 llm_model=$2
 with_dataset_description=$3
 log_file_name=$4
-statistic_file_name=${5}
+statistic_file_name=$5
+data_profile_path=$6
 
 exp_path="$(pwd)"
 data_path="${exp_path}/data"
-data_profile_path="${data_path}/${dataset}/data_profile_full"
-
-
-# Run Data Profiling
-./explocal/exp0_statistics/runExperiment0_Data_Profile.sh ${dataset} ${data_profile_path}
 
 metadata_path="${data_path}/${dataset}/${dataset}.yaml"
 

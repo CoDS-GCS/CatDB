035901dc6aeb751af1f7f41e1ddc9821dffcd539
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-04-15 04:53:19 -0400

    update

--- Experiments/run5LocalExperiments.sh
+++ Experiments/run5LocalExperiments.sh
@@ -22,27 +22,27 @@ echo "dataset,llm_model,prompt_representation_type,prompt_example_type,prompt_nu
 cd ${exp_path}
 
 #CMD=./explocal/exp0_statistics/runExperiment0.sh
-CMD=./explocal/exp1_catalog/runExperiment1.sh
+#CMD=./explocal/exp1_catalog/runExperiment1.sh
 #CMD=./explocal/exp2_micro_benchmark/runExperiment2.sh 
-#CMD=./explocal/exp3_end_to_end/runExperiment3.sh 
+CMD=./explocal/exp3_end_to_end/runExperiment3.sh 
 
 
 # Large Datasets
-$CMD Higgs binary
-$CMD Albert binary
-$CMD Click-Prediction binary
-$CMD Census-Augmented binary
-$CMD Heart-Statlog binary
-$CMD KDDCup99 multiclass
-$CMD Road-Safety multiclass
-$CMD Drug-Directory multiclass
-$CMD Okcupid-Stem multiclass
-$CMD Walking-Activity multiclass
-$CMD PASS regression
-$CMD Aloi regression
-$CMD MD-MIX-Mini regression
-$CMD Dionis regression
-$CMD Meta-Album-BRD regression
+# $CMD Higgs binary
+# $CMD Albert binary
+# $CMD Click-Prediction binary
+# $CMD Census-Augmented binary
+# $CMD Heart-Statlog binary
+# $CMD KDDCup99 multiclass
+# $CMD Road-Safety multiclass
+# $CMD Drug-Directory multiclass
+# $CMD Okcupid-Stem multiclass
+# $CMD Walking-Activity multiclass
+# $CMD PASS regression
+# $CMD Aloi regression
+# $CMD MD-MIX-Mini regression
+# $CMD Dionis regression
+# $CMD Meta-Album-BRD regression
 
 # Small Datasets
 $CMD Balance-Scale multiclass

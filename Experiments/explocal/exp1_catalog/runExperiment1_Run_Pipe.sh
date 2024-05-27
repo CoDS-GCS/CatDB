b6b15c8db2ea6c2b8ab9a8babe5b59b4ecad7784
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-05-27 01:19:02 -0400

    Add Run Code to the Main Script

--- Experiments/explocal/exp1_catalog/runExperiment1_Run_Pipe.sh
+++ Experiments/explocal/exp1_catalog/runExperiment1_Run_Pipe.sh
@@ -3,42 +3,41 @@
 exp_path="$(pwd)"
 data_path="${exp_path}/data"
 dataset=$1
-prompt_representation_type=$2
-prompt_example_type=$3
-prompt_number_example=$4
-task_type=$5
-llm_model=$6
-with_dataset_description=$7
-
+data_profile_path=$2
+prompt_representation_type=$3
+prompt_samples_type=$4
+prompt_number_samples=$5
+task_type=$6
+llm_model=$7
+with_dataset_description=$8
+number_iteration=$9
 metadata_path="${data_path}/${dataset}/${dataset}.yaml"
-number_iteration=20
-parse_pipeline=True
-run_pipeline=True
 
-log_file_name="${exp_path}/results/Experiment1_Run_Pipe.dat"
+log_file_name="${exp_path}/results/Experiment1_Pipe_Run.dat"
 
 output_path="${exp_path}/catdb-results/${dataset}"
 mkdir -p ${output_path}
 
-result_output_path="${exp_path}/results/Experiment1_Run_Pipe_${prompt_representation_type}.dat"
+result_output_path="${exp_path}/results/Experiment1_Pipe_Run_${prompt_representation_type}.csv"
 
 cd "${exp_path}/setup/Baselines/CatDB/"
 source venv/bin/activate
 
-src_path="${output_path}/${llm_model}-${prompt_representation_type}-${prompt_example_type}-${prompt_number_example}-SHOT-${with_dataset_description}.py"
-
-SCRIPT="python -Wignore main_run_code.py --metadata-path ${metadata_path} \
+SCRIPT="python -Wignore main.py --metadata-path ${metadata_path} \
+        --data-profile-path ${data_profile_path} \
         --prompt-representation-type ${prompt_representation_type} \
-        --prompt-example-type ${prompt_example_type} \
-        --prompt-number-example ${prompt_number_example} \
+        --prompt-samples-type ${prompt_samples_type} \
+        --prompt-number-samples ${prompt_number_samples} \
         --prompt-number-iteration ${number_iteration} \
+        --prompt-number-iteration-error 0 \
         --llm-model ${llm_model} \
         --output-path ${output_path} \
-        --parse-pipeline ${parse_pipeline} \
-        --run-pipeline ${run_pipeline} \
         --result-output-path ${result_output_path} \
         --dataset-description ${with_dataset_description} \
-        --src-path ${src_path}"
+        --run-code True"
+
+# sudo echo 3 >/proc/sys/vm/drop_caches && sudo sync
+# sleep 3
 
 echo ${SCRIPT}
 
@@ -46,4 +45,4 @@ start=$(date +%s%N)
 $SCRIPT
 end=$(date +%s%N)
 
-echo "${dataset},${llm_model},${prompt_representation_type},${prompt_example_type},${prompt_number_example},${task_type},$((($end - $start) / 1000000))" >> ${log_file_name}  
+echo "${dataset},${llm_model},${prompt_representation_type},${prompt_samples_type},${prompt_number_samples},${task_type},$((($end - $start) / 1000000))" >> ${log_file_name}  

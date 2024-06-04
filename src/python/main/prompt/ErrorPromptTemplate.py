05e104d26bb5a729703f289e44135a742a5f2b19
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-06-04 15:33:40 -0400

    Minor

--- src/python/main/prompt/ErrorPromptTemplate.py
+++ src/python/main/prompt/ErrorPromptTemplate.py
@@ -2,8 +2,7 @@ from util import StaticValues
 
 
 class BasicErrorPrompt(object):
-    def __init__(self, pipeline_code: str, pipeline_error: str, schema_data: str,
-                 data_source_train_path: str, data_source_test_path: str, *args, **kwargs):
+    def __init__(self, pipeline_code: str, pipeline_error: str, schema_data: str, *args, **kwargs):
         self.rules = []
         self.pipeline_code = pipeline_code
         self.pipeline_error = pipeline_error
@@ -11,8 +10,6 @@ class BasicErrorPrompt(object):
         self.system_message_delimiter = None
         self.user_message_delimiter = None
         self.schema_data = schema_data
-        self.data_source_train_path: data_source_train_path
-        self.data_source_test_path: data_source_test_path
 
     def format_prompt(self):
         return {
@@ -64,14 +61,14 @@ class BasicResultErrorPrompt(object):
 
 
 class RuntimeErrorPrompt(BasicErrorPrompt):
-    def __init__(self, evaluation_text: str, *args, **kwargs):
+    def __init__(self, evaluation_text: str, data_source_train_path: str, data_source_test_path: str, *args, **kwargs):
         BasicErrorPrompt.__init__(self, *args, **kwargs)
         self.rules = ['Task: You are expert in coding assistant. Your task is fix the error of this pipeline code.',
                       'Input: The user will provide a pipeline code enclosed in "<CODE> pipline code will be here. </CODE>", '
                       'and an error message enclosed in "<ERROR> error message will be here. </ERROR>".',
                       f"Rule 1: {StaticValues.rule_code_block}",
                       f"Rule 2: {evaluation_text}",
-                      f'Rule 3 : {StaticValues.dp_rule_2.format(self.data_source_train_path, self.data_source_test_path)}'
+                      f'Rule 3 : {StaticValues.dp_rule_2.format(data_source_train_path, data_source_test_path)}'
                       ]
 
         min_length = min(len(self.pipeline_error), 2000)
@@ -79,12 +76,12 @@ class RuntimeErrorPrompt(BasicErrorPrompt):
 
 
 class ResultsErrorPrompt(BasicResultErrorPrompt):
-    def __init__(self, evaluation_text: str, *args, **kwargs):
+    def __init__(self, evaluation_text: str, data_source_train_path: str, data_source_test_path: str, *args, **kwargs):
         BasicResultErrorPrompt.__init__(self, *args, **kwargs)
         self.rules = ['Task: You are expert in coding assistant. The following results did not achieved by direct '
                       'execution of the pipeline. Modify the code and return achievable results.'
                       'Your task is fix the error and return requested results of this pipeline code.',
                       'Input: The user will provide a pipeline code enclosed in "<CODE> pipline code will be here. </CODE>"',
                       f"Rule 1: {evaluation_text}",
-                      f'Rule 2: {StaticValues.dp_rule_2.format(self.data_source_train_path, self.data_source_test_path)}'
+                      f'Rule 2: {StaticValues.dp_rule_2.format(data_source_train_path, data_source_test_path)}'
                       ]

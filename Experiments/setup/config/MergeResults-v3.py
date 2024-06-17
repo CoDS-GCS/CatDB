ff73f8244762d78af44ff7f8f3b89f548a2c4ec3
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-06-17 00:35:05 -0400

    Merge results

--- Experiments/setup/config/MergeResults-v3.py
+++ Experiments/setup/config/MergeResults-v3.py
@@ -29,13 +29,12 @@ if __name__ == '__main__':
                         "train_f1_score", "train_log_loss", "train_r_squared", "train_rmse", "test_auc","test_auc_ovo",
                         "test_auc_ovr", "test_accuracy", "test_f1_score", "test_log_loss", "test_r_squared", "test_rmse",
                         "prompt_token_count","all_token_count", "operation"]
-    
-    # results_path = [f"{root_path}/Experiment1_LLM_Pipe_Gen_CatDB_1.dat",
-    #                 f"{root_path}/Experiment1_LLM_Pipe_Gen_CatDB_2.dat",
-    #                 f"{root_path}/Experiment1_LLM_Pipe_Gen_CatDBChain_1.dat",
-    #                 f"{root_path}/Experiment1_LLM_Pipe_Gen_CatDBChain_2.dat"] 
-    results_path = [f"{root_path}/Experiment1_LLM_Pipe_Gen_CatDB_2.dat",
-                    f"{root_path}/Experiment1_LLM_Pipe_Gen_CatDBChain_2.dat"] 
+
+    results_path = [f"{root_path}/server-16/part1-CatDB-gemini-1.5-pro-latest/Experiment1_LLM_Pipe_Gen_CatDB.dat",
+                    f"{root_path}/server-35/part1-CatDB-mixtral-llama/Experiment1_LLM_Pipe_Gen_CatDB.dat",
+                    f"{root_path}/server-65/part1-CatDB-gemini-1.5-pro-latest/Experiment1_LLM_Pipe_Gen_CatDB.dat",
+                    f"{root_path}/server-65/part1-CatDB-gemini-1.5-pro-latest/Experiment1_LLM_Pipe_Gen_CatDBChain.dat",
+                    f"{root_path}/server-65/part2-CAFFE-gemini-1.5-pro-latest/Experiment1_LLM_CAAFE.dat"] 
     
     df_pip_gen = pd.DataFrame(columns = columns)
 
@@ -51,50 +50,58 @@ if __name__ == '__main__':
     
     #llms = ["gpt-4", "gpt-4o", "llama3-70b-8192", "gemini-1.5-pro-latest"]
     llms = ["gemini-1.5-pro-latest"]
+    classifier = ["Auto", "TabPFN", "RandomForest"]
 
-    configs = ["CatDB", "CatDBChain"]
+    configs = ["CatDB", "CatDBChain", "CAAFE"]
     
 
     for (ds,ds_id) in datasets:
         for llm in llms:
             for config in configs:
-                #print(f"{config} >> {len(df_pip_gen[]['sub_task'])}")
-                df = df_pip_gen.loc[(df_pip_gen['dataset_name'] == ds) & 
-                                    (df_pip_gen['config'] == config) &
-                                    (df_pip_gen['llm_model'] == llm) &
-                                    (df_pip_gen['status'] == True) &
-                                    ((df_pip_gen['sub_task'] == 'ModelSelection') | (pd.isnull(df_pip_gen['sub_task'])))]
-                
-                
-                if len(df) > 0:
+                for cls in classifier:
+                    #print(f"{config} >> {len(df_pip_gen[]['sub_task'])}")
+                    df = df_pip_gen.loc[(df_pip_gen['dataset_name'] == ds) & 
+                                        (df_pip_gen['config'] == config) &
+                                        (df_pip_gen['llm_model'] == llm) &
+                                        (df_pip_gen['status'] == True) &
+                                        (df_pip_gen['classifier'] == cls) &
+                                        ((df_pip_gen['sub_task'] == 'ModelSelection') | (pd.isnull(df_pip_gen['sub_task'])))]
                     
-                    # Binary Tasks
-                    df_binary = df.loc[(df['train_auc'] >=0) &
-                                (df['test_auc'] >=0) &
-                                (df['task_type'] =='binary')]
-                    df_binary = df_binary.head(10)
-                    df_binary["number_iteration"] = [ki for ki in range(1, len(df_binary)+1)]
-                    mergse_dfs(df_sort, df_binary)
+                    
+                    if len(df) > 0:
+                        
+                        # Binary Tasks
+                        df_binary = df.loc[(df['train_auc'] >=0) &
+                                    (df['test_auc'] >=0) &
+                                    (df['task_type'] =='binary')]
+                        df_binary = df_binary.head(10)
+                        df_binary["number_iteration"] = [ki for ki in range(1, len(df_binary)+1)]
+                        mergse_dfs(df_sort, df_binary)
 
-                    # Multitask Tasks
-                    df_multi = df.loc[(df['train_auc_ovr'] >=0) &
-                                (df['test_auc_ovr'] >=0) &
-                                (df['task_type'] =='multiclass')]
-                    df_multi = df_multi.head(10)
-                    df_multi["number_iteration"] = [ki for ki in range(1, len(df_multi)+1)]
-                    mergse_dfs(df_sort, df_multi)
+                        # Multitask Tasks
+                        df_multi = df.loc[(df['train_auc_ovr'] >=0) &
+                                    (df['test_auc_ovr'] >=0) &
+                                    (df['task_type'] =='multiclass')]
+                        df_multi = df_multi.head(10)
+                        df_multi["number_iteration"] = [ki for ki in range(1, len(df_multi)+1)]
+                        mergse_dfs(df_sort, df_multi)
 
-                    df_ds = df_sort.loc[(df_sort['dataset_name'] ==ds) &
-                                        (df_sort['llm_model'] == llm)&
-                                        (df_sort['config'] == config)]
-                    df_ds["ID"] = [ds_id for dsid in range(0,len(df_ds))]
+                        df_ds = df_sort.loc[(df_sort['dataset_name'] ==ds) &
+                                            (df_sort['llm_model'] == llm)&
+                                            (df_sort['config'] == config)]
+                        df_ds["ID"] = [ds_id for dsid in range(0,len(df_ds))]
 
-                    df_ds.to_csv(f"/home/saeed/Documents/Github/CatDB/Experiments/archive/SIGMOD2025-Results/r/{config}-{ds}-{llm}.csv", index=False)
+                        fname = f"{config}-{ds}-{llm}"
+                        if cls != "Auto":
+                            fname = f"{fname}-{cls}"
+                        fname = f"{fname}.csv"    
+
+                        df_ds.to_csv(f"/home/saeed/Documents/Github/CatDB/Experiments/archive/SIGMOD2025-Results/seperate/{fname}", index=False)
                     
 
 
               
 
-    df_sort.to_csv("/home/saeed/Documents/Github/CatDB/Experiments/archive/SIGMOD2025-Results/r/cc.csv", index=False)
+    df_sort.to_csv("/home/saeed/Documents/Github/CatDB/Experiments/archive/SIGMOD2025-Results/AllResults.csv", index=False)
    
            

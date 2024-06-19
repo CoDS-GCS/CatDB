b143886b24760e461857704523b5aca7a158f4cd
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-06-19 15:37:31 -0400

    clean-up

--- Experiments/explocal/exp3_end_to_end/runExperiment3_AutoML.sh
+++ Experiments/explocal/exp3_end_to_end/runExperiment3_AutoML.sh
@@ -3,7 +3,7 @@
 dataset=$1
 framework=$2
 max_runtime_seconds=$3
-
+iteration=$4
 jvm_memory=20
 
 exp_path="$(pwd)"
@@ -21,15 +21,12 @@ CMD="python -Wignore main${framework}.py --metadata-path ${metadata_path} \
     --max-runtime-seconds ${max_runtime_seconds} \
     --jvm-memory ${jvm_memory} \
     --dataset-path ${exp_path} \
-    --output-dir ${output_dir}"
-
-
-# $CMD --automl-framework H2O
-# $CMD --automl-framework FLAML
-# $CMD --automl-framework Autogluon
+    --output-dir ${output_dir} \
+    --iteration ${iteration}"
 
 cd "${framework}AutoML"
 source venv/bin/activate
 cd ..
 
+echo ${CMD}
 $CMD

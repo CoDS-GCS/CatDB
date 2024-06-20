b7d7f7aa658bac15c1e1eddc01614626a053af09
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-06-20 15:22:40 -0400

    update results

--- Experiments/run5LocalExperiments.sh
+++ Experiments/run5LocalExperiments.sh
@@ -22,57 +22,57 @@ CMD=./explocal/exp1_catalog/runExperiment1.sh
 
 
 $CMD Balance-Scale multiclass # Balance-Scale
-$CMD Breast-w binary # Breast-w
-$CMD CMC multiclass # CMC
-$CMD Credit-g binary # Credit-g
-$CMD Diabetes binary # Diabetes
-$CMD Tic-Tac-Toe binary # Tic-Tac-Toe
-$CMD Eucalyptus multiclass # Eucalyptus
-$CMD PC1 binary # PC1
-$CMD Airlines binary # Airlines
-$CMD Jungle-Chess multiclass # Jungle-Chess
+# $CMD Breast-w binary # Breast-w
+# $CMD CMC multiclass # CMC
+# $CMD Credit-g binary # Credit-g
+# $CMD Diabetes binary # Diabetes
+# $CMD Tic-Tac-Toe binary # Tic-Tac-Toe
+# $CMD Eucalyptus multiclass # Eucalyptus
+# $CMD PC1 binary # PC1
+# $CMD Airlines binary # Airlines
+# $CMD Jungle-Chess multiclass # Jungle-Chess
 
-$CMD Higgs binary # Higgs
-$CMD Skin binary # Skin
-$CMD Click-Prediction binary # Click-Prediction
-$CMD Census-Augmented binary # Census-Augmented
-$CMD Heart-Statlog binary # Heart-Statlog
-$CMD KDDCup99 multiclass # KDDCup99
-$CMD Road-Safety multiclass # Road-Safety
-$CMD Drug-Directory multiclass # Drug-Directory
-$CMD Traffic multiclass # Traffic
-$CMD Walking-Activity multiclass # Walking-Activity
-$CMD Black-Friday regression # Black-Friday
-$CMD Bike-Sharing regression # Bike-Sharing
-$CMD House-Sales regression # House-Sales
-$CMD NYC regression # NYC
-$CMD Airlines-DepDelay regression # Airlines-DepDelay
-$CMD Adult binary # Adult
+# $CMD Higgs binary # Higgs
+# $CMD Skin binary # Skin
+# $CMD Click-Prediction binary # Click-Prediction
+# $CMD Census-Augmented binary # Census-Augmented
+# $CMD Heart-Statlog binary # Heart-Statlog
+# $CMD KDDCup99 multiclass # KDDCup99
+# $CMD Road-Safety multiclass # Road-Safety
+# $CMD Drug-Directory multiclass # Drug-Directory
+# $CMD Traffic multiclass # Traffic
+# $CMD Walking-Activity multiclass # Walking-Activity
+# $CMD Black-Friday regression # Black-Friday
+# $CMD Bike-Sharing regression # Bike-Sharing
+# $CMD House-Sales regression # House-Sales
+# $CMD NYC regression # NYC
+# $CMD Airlines-DepDelay regression # Airlines-DepDelay
+# $CMD Adult binary # Adult
 
-$CMD oml_dataset_1_rnc multiclass # Balance-Scale
-$CMD oml_dataset_2_rnc binary # Breast-w
-$CMD oml_dataset_3_rnc multiclass # CMC
-$CMD oml_dataset_4_rnc binary # Credit-g
-$CMD oml_dataset_5_rnc binary # Diabetes
-$CMD oml_dataset_6_rnc binary # Tic-Tac-Toe
-$CMD oml_dataset_7_rnc multiclass # Eucalyptus
-$CMD oml_dataset_8_rnc binary # PC1
-$CMD oml_dataset_9_rnc binary # Airlines
-$CMD oml_dataset_10_rnc multiclass # Jungle-Chess
+# $CMD oml_dataset_1_rnc multiclass # Balance-Scale
+# $CMD oml_dataset_2_rnc binary # Breast-w
+# $CMD oml_dataset_3_rnc multiclass # CMC
+# $CMD oml_dataset_4_rnc binary # Credit-g
+# $CMD oml_dataset_5_rnc binary # Diabetes
+# $CMD oml_dataset_6_rnc binary # Tic-Tac-Toe
+# $CMD oml_dataset_7_rnc multiclass # Eucalyptus
+# $CMD oml_dataset_8_rnc binary # PC1
+# $CMD oml_dataset_9_rnc binary # Airlines
+# $CMD oml_dataset_10_rnc multiclass # Jungle-Chess
 
-$CMD oml_dataset_11_rnc binary # Higgs
-$CMD oml_dataset_12_rnc binary # Skin
-$CMD oml_dataset_13_rnc binary # Click-Prediction
-$CMD oml_dataset_14_rnc binary # Census-Augmented
-$CMD oml_dataset_15_rnc binary # Heart-Statlog
-$CMD oml_dataset_16_rnc multiclass # KDDCup99
-$CMD oml_dataset_17_rnc multiclass # Road-Safety
-$CMD oml_dataset_18_rnc multiclass # Drug-Directory
-$CMD oml_dataset_19_rnc multiclass # Traffic
-$CMD oml_dataset_20_rnc multiclass # Walking-Activity
-$CMD oml_dataset_21_rnc regression # Black-Friday
-$CMD oml_dataset_22_rnc regression # Bike-Sharing
-$CMD oml_dataset_23_rnc regression # House-Sales
-$CMD oml_dataset_24_rnc regression # NYC
-$CMD oml_dataset_25_rnc regression # Airlines-DepDelay
-$CMD oml_dataset_26_rnc binary # Adult
+# $CMD oml_dataset_11_rnc binary # Higgs
+# $CMD oml_dataset_12_rnc binary # Skin
+# $CMD oml_dataset_13_rnc binary # Click-Prediction
+# $CMD oml_dataset_14_rnc binary # Census-Augmented
+# $CMD oml_dataset_15_rnc binary # Heart-Statlog
+# $CMD oml_dataset_16_rnc multiclass # KDDCup99
+# $CMD oml_dataset_17_rnc multiclass # Road-Safety
+# $CMD oml_dataset_18_rnc multiclass # Drug-Directory
+# $CMD oml_dataset_19_rnc multiclass # Traffic
+# $CMD oml_dataset_20_rnc multiclass # Walking-Activity
+# $CMD oml_dataset_21_rnc regression # Black-Friday
+# $CMD oml_dataset_22_rnc regression # Bike-Sharing
+# $CMD oml_dataset_23_rnc regression # House-Sales
+# $CMD oml_dataset_24_rnc regression # NYC
+# $CMD oml_dataset_25_rnc regression # Airlines-DepDelay
+# $CMD oml_dataset_26_rnc binary # Adult

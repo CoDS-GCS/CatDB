8ab4cfd778a9cbce15dabe6f2ecc0dd62417ce4a
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-07-03 16:26:25 -0400

    minor

--- Experiments/explocal/exp3_end_to_end/runExperiment3.sh
+++ Experiments/explocal/exp3_end_to_end/runExperiment3.sh
@@ -6,26 +6,27 @@ task_type=$2
 # Run AutoML Baseline
 iteration=1
 deafult_max_run_time=1
+CMD=./explocal/exp3_end_to_end/runExperiment3_AutoML.sh
 
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset AutoSklearn ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDBChain
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset H2O  ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDBChain
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset Flaml  ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDBChain
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset Autogluon ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDBChain
-
-
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset AutoSklearn ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDBChain
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset H2O  ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDBChain
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset Flaml  ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDBChain
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset Autogluon ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDBChain
+$CMD $dataset AutoSklearn ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDBChain
+$CMD $dataset H2O  ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDBChain
+$CMD $dataset Flaml  ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDBChain
+$CMD $dataset Autogluon ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDBChain
 
 
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset AutoSklearn ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDB
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset H2O  ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDB
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset Flaml  ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDB
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset Autogluon ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDB
+$CMD $dataset AutoSklearn ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDBChain
+$CMD $dataset H2O  ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDBChain
+$CMD $dataset Flaml  ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDBChain
+$CMD $dataset Autogluon ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDBChain
 
 
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset AutoSklearn ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDB
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset H2O  ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDB
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset Flaml  ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDB
-./explocal/exp3_end_to_end/runExperiment3_AutoML.sh $dataset Autogluon ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDB
+$CMD $dataset AutoSklearn ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDB
+$CMD $dataset H2O  ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDB
+$CMD $dataset Flaml  ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDB
+$CMD $dataset Autogluon ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest CatDB
+
+
+$CMD $dataset AutoSklearn ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDB
+$CMD $dataset H2O  ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDB
+$CMD $dataset Flaml  ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDB
+$CMD $dataset Autogluon ${deafult_max_run_time} ${iteration} llama3-70b-8192 CatDB

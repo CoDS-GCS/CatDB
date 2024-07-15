3724bd57c531faab9409e40404136b476bfb3430
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-07-15 16:28:30 -0400

    update setting

--- Experiments/explocal/exp3_end_to_end/runExperiment3.sh
+++ Experiments/explocal/exp3_end_to_end/runExperiment3.sh
@@ -14,13 +14,13 @@ $CMD $dataset Flaml  ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest
 $CMD $dataset Autogluon ${deafult_max_run_time} ${iteration} gemini-1.5-pro-latest 
 
 
-$CMD $dataset AutoSklearn ${deafult_max_run_time} ${iteration} llama3-70b-8192 
-$CMD $dataset H2O  ${deafult_max_run_time} ${iteration} llama3-70b-8192 
-$CMD $dataset Flaml  ${deafult_max_run_time} ${iteration} llama3-70b-8192 
-$CMD $dataset Autogluon ${deafult_max_run_time} ${iteration} llama3-70b-8192 
+# $CMD $dataset AutoSklearn ${deafult_max_run_time} ${iteration} llama3-70b-8192 
+# $CMD $dataset H2O  ${deafult_max_run_time} ${iteration} llama3-70b-8192 
+# $CMD $dataset Flaml  ${deafult_max_run_time} ${iteration} llama3-70b-8192 
+# $CMD $dataset Autogluon ${deafult_max_run_time} ${iteration} llama3-70b-8192 
 
 
-$CMD $dataset AutoSklearn ${deafult_max_run_time} ${iteration} gpt-4o 
-$CMD $dataset H2O  ${deafult_max_run_time} ${iteration} gpt-4o 
-$CMD $dataset Flaml  ${deafult_max_run_time} ${iteration} gpt-4o 
-$CMD $dataset Autogluon ${deafult_max_run_time} ${iteration} gpt-4o 
+# $CMD $dataset AutoSklearn ${deafult_max_run_time} ${iteration} gpt-4o 
+# $CMD $dataset H2O  ${deafult_max_run_time} ${iteration} gpt-4o 
+# $CMD $dataset Flaml  ${deafult_max_run_time} ${iteration} gpt-4o 
+# $CMD $dataset Autogluon ${deafult_max_run_time} ${iteration} gpt-4o 

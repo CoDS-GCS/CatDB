9aa628a68b872de805d07eaa6a8d3355b43063d8
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-08-13 23:25:43 -0400

    Update Script

--- Experiments/run2SetupBaseLines.sh
+++ Experiments/run2SetupBaseLines.sh
@@ -33,7 +33,7 @@ cd $path
 # Setup CatDB
 #############
 catdb_path="${path}/Baselines/CatDB/"
-#rm -rf ${catdb_path}
+rm -rf ${catdb_path}
 mkdir -p ${catdb_path}
 
 cd ${root_path}
@@ -41,28 +41,28 @@ cd ..
 cp -r src/python/main/* ${catdb_path}
 cd ${catdb_path}
 
-# rm -rf venv 
-# python -m venv venv
-# source venv/bin/activate
+rm -rf venv 
+python -m venv venv
+source venv/bin/activate
 
-# # Then install the dependencies:
-# python -m pip install --upgrade pip
-# pip install torchvision 
-# python -m pip install -r requirements.txt
-# pip install pipreqs
+# Then install the dependencies:
+python -m pip install --upgrade pip
+pip install torchvision 
+python -m pip install -r requirements.txt
+pip install pipreqs
 
 # Prepare Config
 ################
-# config_path="${path}/config/"
-# cd ${config_path}
-# rm -rf venv
+config_path="${path}/config/"
+cd ${config_path}
+rm -rf venv
 
-# python -m venv venv
-# source venv/bin/activate
+python -m venv venv
+source venv/bin/activate
 
-# #Then install the dependencies:
-# python -m pip install --upgrade pip
-# python -m pip install -r requirements.txt
+#Then install the dependencies:
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 # Setup CAAFE
 #############

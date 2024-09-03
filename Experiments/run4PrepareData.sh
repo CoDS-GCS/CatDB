1f87bbc53eb446f44e7bdb9e5e2960a9c888b23a
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-09-03 00:16:31 -0400

    Update data prepare script

--- Experiments/run4PrepareData.sh
+++ Experiments/run4PrepareData.sh
@@ -5,6 +5,18 @@ data_path="/home/saeed/Downloads/RDB-Export/"
 data_out_path="${root_path}/data"
 config_path="${root_path}/setup/config"
 
+cd ${data_path}
+unzip Accidents.zip  
+unzip Airline.zip 
+unzip Financial.zip
+unzip Hockey.zip 
+unzip IMDB_IJS.zip
+unzip IMDB.zip
+unzip Lahman-2014.zip
+unzip Walmart-2014.zip
+unzip Walmart.zip
+unzip Yelp.zip
+
 cd ${config_path}
 source venv/bin/activate
 
@@ -12,6 +24,7 @@ CMD="python DatasetPrepare.py --dataset-root-path ${data_path} \
         --multi-table True \
         --data-out-path ${data_out_path}"
 
+ 
 $CMD --dataset-name Accidents --target-attribute klas_nesreca --task-type multiclass --target-table nesreca 
 
 cd ${root_path}

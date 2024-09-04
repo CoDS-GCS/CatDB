60b04949a9f12652f1d70097a310494d187bed97
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-09-04 14:39:45 -0400

    Add Train, Test, and Verify dataset

--- Experiments/run4PrepareData.sh
+++ Experiments/run4PrepareData.sh
@@ -9,8 +9,8 @@ cd ${data_path}
 unzip Accidents.zip  
 unzip Airline.zip 
 unzip Financial.zip
-unzip Hockey.zip 
-unzip IMDB_IJS.zip
+# unzip Hockey.zip 
+unzip IMDB-IJS.zip
 unzip IMDB.zip
 unzip Lahman-2014.zip
 unzip Walmart-2014.zip
@@ -25,6 +25,14 @@ CMD="python DatasetPrepare.py --dataset-root-path ${data_path} \
         --data-out-path ${data_out_path}"
 
  
-$CMD --dataset-name Accidents --target-attribute klas_nesreca --task-type multiclass --target-table nesreca 
+# $CMD --dataset-name Accidents --target-attribute klas_nesreca --task-type multiclass --target-table nesreca 
+# $CMD --dataset-name Airline --target-attribute ArrDel15 --task-type multiclass --target-table On_Time_On_Time_Performance_2016_1 
+# $CMD --dataset-name Financial --target-attribute status --task-type multiclass --target-table loan 
+# $CMD --dataset-name IMDB-IJS --target-attribute gender --task-type binary --target-table actors 
+# $CMD --dataset-name IMDB --target-attribute sex --task-type binary --target-table actors 
+# $CMD --dataset-name Lahman-2014 --target-attribute salary --task-type regression --target-table salaries 
+# #$CMD --dataset-name Walmart-2014 --target-attribute klas_nesreca --task-type regression --target-table nesreca 
+# $CMD --dataset-name Walmart --target-attribute units --task-type regression --target-table train 
+# $CMD --dataset-name Yelp --target-attribute stars --task-type regression --target-table Reviews 
 
 cd ${root_path}

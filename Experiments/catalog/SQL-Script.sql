73fc3d50093bb41365a03f0b2d7db1bec6df1913
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-09-04 14:10:48 -0400

    add data profile infor for multi-table datasets

---
+++ Experiments/catalog/SQL-Script.sql
@@ -0,0 +1,18 @@
+select concat(tab.table_name,':','PK: ',sta.column_name,' FK: ')
+
+from information_schema.tables as tab
+inner join information_schema.statistics as sta
+        on sta.table_schema = tab.table_schema
+        and sta.table_name = tab.table_name
+        and sta.index_name = 'PRIMARY'
+where tab.table_schema = 'Yelp'
+    and tab.table_type = 'BASE TABLE'
+order by tab.table_name;
+
+
+SELECT
+    concat(TABLE_NAME,': (', COLUMN_NAME,') references ', REFERENCED_TABLE_NAME, ' (',REFERENCED_COLUMN_NAME,'),')
+FROM
+  INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE
+  REFERENCED_TABLE_SCHEMA = 'Yelp' 

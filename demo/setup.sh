607ea3e4a99a9b59ea3509ed91b38c5773e8b470
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-10-27 00:57:40 -0400

    add demo setup script

---
+++ demo/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Create a setup path and install CatDB
+!mkdir -p catdb-setup
+# !cp -r CatDB/src/python/main/* catdb-setup
+# %cd catdb-setup/
+# !apt install python3.10-venv
+# !rm -rf venv # clean-up last env 
+# !python3.10 -m venv venv
+# !source venv/bin/activate
+
+# # Then install the dependencies:
+# !python3.10 -m pip install --upgrade pip
+# !pip install torchvision 
+# !python3.10 -m pip install -r requirements.txt
+
+# # Prepare demo data
+# !apt install -y unzip
+# !cp -r CatDB/demo-data ../
+# %cd /content/demo-data
+# !unzip catalog.zip
+# !unzip data.zip
+# !mkdir catdb-results
+
+# # Clean-up
+# !rm -rf /content/CatDB

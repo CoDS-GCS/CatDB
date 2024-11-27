c761d2aceaa6d81e130d31940b5faf39d1aa1e38
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-11-27 01:40:28 -0500

    minor

--- CatDB/core.py
+++ CatDB/core.py
@@ -1,4 +1,4 @@
-from ..src.python.main.catalog.Catalog import load_data_source_profile
+from src.python.main.catalog.Catalog import load_data_source_profile
 # from catalog.Dependency import load_dependency_info
 # from pipegen.GeneratePipeLine import generate_and_verify_pipeline, run_pipeline, clean_categorical_data
 # from util.FileHandler import read_text_file_line_by_line

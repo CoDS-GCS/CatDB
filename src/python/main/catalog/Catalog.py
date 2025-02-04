758121d92f7b8add7ee399ac59361d882b8a9d2a
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2025-02-04 17:50:47 -0500

    Add Top-K Implementation for Extra Experiments

--- src/python/main/catalog/Catalog.py
+++ src/python/main/catalog/Catalog.py
@@ -1,4 +1,9 @@
 import os
+import pandas as pd
+import multiprocessing
+import threading
+import math
+import concurrent.futures
 from .Profile import load_JSON_profile_info, load_JSON_profile_info_with_update
 from .DimensionReduction import ReduceDimension
 from .Dependency import Dependency
@@ -171,6 +176,224 @@ def load_data_source_profile(data_source_path: str, file_format: str, target_att
                        )
 
 
+def load_data_source_profile_TopK(data_source_path: str, file_format: str, target_attribute: str, enable_reduction: bool,
+                             dependency: Dependency=None, categorical_values_restricted_size: int=50, cleaning: bool=False, k:int=10):
+    profile_info = dict()
+    schema_info = dict()
+    ncols = 0
+    nrows = 0
+    dataset_name = None
+    source_path = None
+    schema_info_group = dict()
+    col_group_name = dict()
+
+    columns_categorical = []
+    columns_categorical_missing_values = []
+    columns_numerical = []
+    columns_numerical_missing_values = []
+    columns_bool = []
+    columns_bool_missing_values = []
+    columns_others = []
+    columns_others_missing_values = []
+    table_name = None
+    embedding = dict()
+
+    for d in os.listdir(data_source_path):
+        files = [f for f in os.listdir(f'{data_source_path}/{d}/')]
+        for f in files:
+            if cleaning:
+                profile = load_JSON_profile_info(f'{data_source_path}/{d}/{f}',
+                                                 categorical_values_restricted_size=categorical_values_restricted_size)
+            else:
+                profile = load_JSON_profile_info_with_update( data_profile_update=f'{data_source_path}_update',
+                                                 file_name=f'{data_source_path}/{d}/{f}')
+            table_name = profile.table_name
+            profile_info[profile.column_name] = profile
+            schema_info[profile.column_name] = profile.short_data_type
+
+            if schema_info_group.get(profile.short_data_type) is None:
+                schema_info_group[profile.short_data_type] = []
+            else:
+                schema_info_group[profile.short_data_type].append(profile.column_name)
+            col_group_name[profile.column_name] = profile.short_data_type
+
+            embedding[profile.column_name] = profile.embedding
+
+            nrows = profile.nrows
+            ncols += 1
+            if dataset_name is None:
+                dataset_name = profile.dataset_name
+                source_path = profile.path
+
+            if profile.short_data_type == 'list':
+                continue
+
+            if profile.is_categorical and profile.short_data_type != 'bool':
+                columns_categorical.append(profile.column_name)
+                if profile.missing_values_count > 0 and profile.column_name != target_attribute:
+                    columns_categorical_missing_values.append(profile.column_name)
+
+            elif profile.short_data_type in {"int", "float"}:
+                columns_numerical.append(profile.column_name)
+                if profile.missing_values_count > 0 and profile.column_name != target_attribute:
+                    columns_numerical_missing_values.append(profile.column_name)
+
+            elif profile.short_data_type == "bool":
+                columns_bool.append(profile.column_name)
+                if profile.missing_values_count > 0 and profile.column_name != target_attribute:
+                    columns_bool_missing_values.append(profile.column_name)
+
+            else:
+                columns_others.append(profile.column_name)
+                if profile.missing_values_count > 0 and profile.column_name != target_attribute:
+                    columns_others_missing_values.append(profile.column_name)
+
+    orig_profile_size = len(schema_info)
+    drop_schema_info = dict()
+    if enable_reduction:
+        rd = ReduceDimension(profile_info=profile_info, target_attribute=target_attribute)
+
+        schema_info, schema_info_group, drop_schema_info, profile_info = rd.get_new_profile_info()
+        new_profile_size = len(schema_info)
+
+        for dk in drop_schema_info.keys():
+            if dk in columns_categorical:
+                columns_categorical.remove(dk)
+                if dk in columns_categorical_missing_values:
+                    columns_categorical_missing_values.remove(dk)
+
+            if dk in columns_numerical:
+                columns_numerical.remove(dk)
+                if dk in columns_numerical_missing_values:
+                    columns_numerical_missing_values.remove(dk)
+
+            if dk in columns_bool:
+                columns_bool.remove(dk)
+                if dk in columns_bool_missing_values:
+                    columns_bool_missing_values.remove(dk)
+
+            if dk in columns_others:
+                columns_others .remove(dk)
+                if dk in columns_others_missing_values:
+                    columns_others_missing_values.remove(dk)
+    # TODO: this code should merge with privious implementation
+    # Compute to select Top-K columns
+    # orders: categorical, corrolation
+
+    profile_info_topk = dict()
+    schema_info_topk = dict()
+    schema_info_group_topk = dict()
+    drop_schema_info_topk = dict()
+
+    columns_categorical_topk = []
+    columns_categorical_missing_values_topk = []
+    columns_numerical_topk = []
+    columns_numerical_missing_values_topk = []
+    columns_bool_topk = []
+    columns_bool_missing_values_topk = []
+    columns_others_topk = []
+    columns_others_missing_values_topk = []
+
+    ck = 0
+    if len(columns_categorical) < k:
+        columns_categorical_topk = columns_categorical
+        columns_categorical_missing_values_topk = columns_categorical_missing_values
+    else:
+        ck = min(k, len(columns_categorical))
+        for col in calc_corrolation(cols=columns_categorical, k = ck, embedding=embedding):
+            columns_categorical_topk.append(col)
+            if col in columns_categorical_missing_values:
+                columns_categorical_missing_values_topk.append(ch)
+    # if ck < k:
+    #     for col in columns_bool:
+    #         columns_bool_topk.append(col)
+    #         if col in columns_bool_missing_values:
+    #             columns_bool_missing_values_topk.append(col)
+    #         ck += 1
+    #         if ck > k:
+    #             break
+    if ck < k:
+        ck = min(k-ck, len(columns_others)+len(columns_numerical))
+        cols = []
+        cols.extend(columns_numerical)
+        cols.extend(columns_others)
+        for col in calc_corrolation(cols=cols, k=ck, embedding=embedding):
+            if col in columns_numerical:
+                columns_numerical_topk.append(col)
+                if col in columns_categorical_missing_values:
+                    columns_numerical_missing_values_topk.append(ch)
+            else:
+                for col in columns_others:
+                    columns_others_topk.append(col)
+                    if col in columns_others_missing_values:
+                        columns_others_missing_values_topk.append(col)
+    topk_cols = [target_attribute]
+    topk_cols.extend(columns_bool_topk)
+    topk_cols.extend(columns_numerical_topk)
+    topk_cols.extend(columns_others_topk)
+    topk_cols.extend(columns_categorical_topk)
+    for col in topk_cols:
+        schema_info_group_topk[col_group_name[col]] = col
+        schema_info_topk[col] = schema_info[col]
+        profile_info_topk[col] = profile_info[col]
+
+    return CatalogInfo(nrows=nrows,
+                       ncols=ncols,
+                       file_format="csv",
+                       dataset_name=dataset_name,
+                       table_name=table_name,
+                       schema_info=schema_info_topk,
+                       profile_info=profile_info_topk,
+                       data_source_path=source_path,
+                       drop_schema_info=drop_schema_info,
+                       schema_info_group=schema_info_group_topk,
+                       columns_categorical=columns_categorical_topk,
+                       columns_categorical_missing_values=columns_categorical_missing_values_topk,
+                       columns_numerical=columns_numerical_topk,
+                       columns_numerical_missing_values=columns_numerical_missing_values_topk,
+                       columns_bool=columns_bool_topk,
+                       columns_bool_missing_values=columns_bool_missing_values_topk,
+                       columns_others=columns_others_topk,
+                       columns_others_missing_values=columns_others_missing_values_topk,
+                       dependency=dependency
+                       )
+
+
+def calc_euclidean_distance(list1, list2):
+    return sum((p - q) ** 2 for p, q in zip(list1, list2)) ** .5
+
+def calc_embedding_euclidean_distance_similarity(embedding, keys: [], indexi: int, indexj: int, indexj_end: int):
+        df_distance = pd.DataFrame(columns=["col","distance"])
+        xi = embedding[keys[indexi]]
+        for j in range(indexj, indexj_end):
+            ed = calc_euclidean_distance(xi, embedding[keys[j]])
+            df_distance.loc[len(df_distance)] = [keys[j], ed]
+        return df_distance
+def calc_corrolation(cols, embedding, k):
+    df_distance = pd.DataFrame(columns=["col", "distance"])
+    number_threads = multiprocessing.cpu_count()
+    len_keys = len(cols)
+    for indexi in range(0, len_keys - number_threads):
+        threads = list()
+        blklen = int(math.ceil((len_keys - indexi - 1) / number_threads))
+        for i in range(0, number_threads):
+            if i * blklen < len_keys:
+                indexj = i * blklen + indexi + 1
+                indexj_end = min((i + 1) * blklen + indexi + 1, len_keys)
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    t = executor.submit(calc_embedding_euclidean_distance_similarity, embedding ,cols, indexi, indexj, indexj_end)
+                    threads.append(t)
+            else:
+                break
+
+        for index, thread in enumerate(threads):
+            ed = thread.result()
+            for index, row in ed.iterrows():
+                df_distance.loc[len(df_distance)] = row
+    df_distance = df_distance.sort_values(by='distance', ascending=False).reset_index(drop=True)
+    df_distance = df_distance.head(k)
+    return df_distance["col"].tolist()
+
 def load_data_source_profile_as_chunck(data_source_path: str, file_format: str, target_attribute: str, enable_reduction: bool,
                              dependency: Dependency=None, categorical_values_restricted_size: int=50, cleaning: bool=False, chunk_size:int=1):
     profile_info = dict()
@@ -210,7 +433,7 @@ def load_data_source_profile_as_chunck(data_source_path: str, file_format: str, 
                 schema_info_group[profile.short_data_type] = []
             else:
                 schema_info_group[profile.short_data_type].append(profile.column_name)
-                col_group_name[profile.column_name] = profile.short_data_type
+            col_group_name[profile.column_name] = profile.short_data_type
 
             nrows = profile.nrows
             ncols += 1
---
+++ src/python/main/main_TopK.py
@@ -0,0 +1,130 @@
+from argparse import ArgumentParser
+from catalog.Catalog import load_data_source_profile_TopK
+from catalog.Dependency import load_dependency_info
+from pipegen.GeneratePipeLine import generate_and_verify_pipeline
+from runcode.RunLocalPipeLine import run_local_pipeline_code
+from util.Config import load_config
+from util.FileHandler import read_text_file_line_by_line
+import time
+import yaml
+
+def parse_arguments():
+    from util.Config import CATEGORICAL_VALUES_RESTRICTED_SIZE
+    parser = ArgumentParser()
+    parser.add_argument('--metadata-path', type=str, default=None)
+    parser.add_argument('--root-data-path', type=str, default=None)
+    parser.add_argument('--catalog-path', type=str, default=None)
+    parser.add_argument('--dataset-description', type=str, default="yes")
+    parser.add_argument('--prompt-representation-type', type=str, default=None)
+    parser.add_argument('--prompt-samples-type', type=str, default=None)
+    parser.add_argument('--prompt-number-samples', type=int, default=None)
+    parser.add_argument('--prompt-number-iteration', type=int, default=1)
+    parser.add_argument('--prompt-number-iteration-error', type=int, default=1)
+    parser.add_argument('--output-path', type=str, default=None)
+    parser.add_argument('--llm-model', type=str, default=None)
+    parser.add_argument('--enable-reduction', type=bool, default=False)
+    parser.add_argument('--result-output-path', type=str, default="/tmp/results.csv")
+    parser.add_argument('--error-output-path', type=str, default="/tmp/catdb_error.csv")
+    parser.add_argument('--run-code', type=bool, default=False)
+    parser.add_argument('--system-log', type=str, default="/tmp/catdb-system-log.dat")
+    parser.add_argument('--topk', type=int, default=10)
+    parser.add_argument('--categorical-values-restricted-size', type=int, default=CATEGORICAL_VALUES_RESTRICTED_SIZE)
+
+    args = parser.parse_args()
+
+    if args.metadata_path is None:
+        raise Exception("--metadata-path is a required parameter!")
+
+    if args.root_data_path is None:
+        raise Exception("--root-data-path is a required parameter!")
+
+    if args.catalog_path is None:
+        raise Exception("--catalog-path is a required parameter!")
+
+    # read .yaml file and extract values:
+    with open(args.metadata_path, "r") as f:
+        try:
+            config_data = yaml.load(f, Loader=yaml.FullLoader)
+            args.dataset_name = config_data[0].get('name')
+            args.target_attribute = config_data[0].get('dataset').get('target')
+            args.task_type = config_data[0].get('dataset').get('type')
+            args.multi_table = config_data[0].get('dataset').get('multi_table')
+            args.target_table = config_data[0].get('dataset').get('target_table')
+            if args.multi_table is None or args.multi_table not in {True, False}:
+                args.multi_table = False
+
+            try:
+                args.data_source_path = f"{args.root_data_path}/{args.dataset_name}/{args.dataset_name}.csv"
+                args.data_source_train_path = f"{args.root_data_path}/{config_data[0].get('dataset').get('train')}"
+                args.data_source_test_path = f"{args.root_data_path}/{config_data[0].get('dataset').get('test')}"
+                args.data_source_verify_path = f"{args.root_data_path}/{config_data[0].get('dataset').get('train')}"
+                args.data_source_train_clean_path = f"{args.data_source_train_path.replace('.csv','')}_clean.csv"
+                args.data_source_test_clean_path = f"{args.data_source_test_path.replace('.csv','')}_clean.csv"
+                args.data_source_verify_clean_path = f"{args.data_source_verify_path.replace('.csv','')}_clean.csv"
+                args.data_source_clean_path = f"{args.data_source_path.replace('.csv', '')}_clean.csv"
+
+            except Exception as ex:
+                raise Exception(ex)
+
+        except yaml.YAMLError as ex:
+            raise Exception(ex)
+
+    if args.prompt_samples_type is None:
+        raise Exception("--prompt-sample-type is a required parameter!")
+
+    if args.prompt_number_samples is None:
+        raise Exception("--prompt-number-samples is a required parameter!")
+
+    if args.llm_model is None:
+        raise Exception("--llm-model is a required parameter!")
+
+    if args.prompt_number_iteration is None:
+        args.prompt_number_iteration = 1
+
+    if args.prompt_representation_type in {"CatDB", "CatDBChain"}:
+        args.enable_reduction = True
+
+    if args.dataset_description.lower() == "yes":
+        dataset_description_path = args.metadata_path.replace(".yaml", ".txt")
+        args.description = read_text_file_line_by_line(fname=dataset_description_path)
+        args.dataset_description = 'Yes'
+    else:
+        args.description = None
+        args.dataset_description = 'No'
+
+    return args
+
+
+
+if __name__ == '__main__':
+    from util.Config import __execute_mode, __gen_verify_mode, __sub_task_data_preprocessing, \
+        __sub_task_feature_engineering, __sub_task_model_selection
+
+    args = parse_arguments()
+    begin_iteration = 1
+    end_iteration = 1
+
+    data_profile_path = f"{args.catalog_path}/data_profile"
+    time_start = time.time()
+    dependency_file = f"{args.catalog_path}/dependency.yaml"
+    dependencies = load_dependency_info(dependency_file=dependency_file, datasource_name=args.dataset_name)
+    load_config(system_log=args.system_log, llm_model=args.llm_model, rules_path="Rules.yaml", evaluation_acc=False,
+                api_config_path="APIKeys.yaml", enable_cache=False)
+    catalog = load_data_source_profile_TopK(data_source_path=data_profile_path,
+                                                file_format="JSON",
+                                                target_attribute=args.target_attribute,
+                                                enable_reduction=args.enable_reduction,
+                                                categorical_values_restricted_size=-1, k=args.topk)
+    time_end = time.time()
+    time_catalog = time_end - time_start
+    ti = 0
+    t = args.prompt_number_iteration * 2
+    while begin_iteration < args.prompt_number_iteration + end_iteration:
+        final_status, code = generate_and_verify_pipeline(args=args, catalog=[catalog], run_mode=__execute_mode,
+                            time_catalog=time_catalog, iteration=begin_iteration, dependency=dependencies)
+        if final_status:
+            begin_iteration += 1
+
+        ti += 1
+        if ti > t:
+            break
